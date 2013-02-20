294a676b1f615b61eef7b340d82cbc9f7418229f
Sergey Khasanov <s.a.khasanov@gmail.com>
2013-02-21 00:47:45 +0700

    C++/Qt version, for python version see branch python-prototype

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#if QT_VERSION < 0x050000
+    #include <QTextCodec>
+#endif
+
+//#include <QTranslator>
+//#include <QLibraryInfo>
+
+#include <QApplication>
+
+#include "MainWidget.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+#if QT_VERSION < 0x050000
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+#endif
+
+    //QTranslator qtTranslator;
+    //qtTranslator.load("qt_ru", QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    //app.installTranslator(&qtTranslator);
+
+    MainWidget w;
+    w.setMinimumSize(w.sizeHint());
+    w.show();
+
+    return app.exec();
+}
